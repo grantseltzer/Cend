68236a0e87ea55831aab2af6cd2e995385e43355
Grant Selter Rchman <grantseltzer@gmail.com>
2015-06-03 19:23:49 -0400

    Update transfer.sh
    Made it so that it would work for entire directories not just individual files. Just added -r flag.

--- transfer.sh
+++ transfer.sh
@@ -9,4 +9,4 @@ read profile
 cd /home/$USER/.cend
 profileInfo=$(<user-$profile.txt)
 IFS=" " read prof ip un pw path<<< $profileInfo
-sshpass -p $pw scp $cwd/$file $un@$ip:$path
+sshpass -p $pw scp -r $cwd/$file $un@$ip:$path

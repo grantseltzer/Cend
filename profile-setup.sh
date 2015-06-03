c77400097ab4617c9c71ff58d14b63ac54a8d902
Grant Selter Rchman <grantseltzer@gmail.com>
2015-06-02 23:39:45 -0400

    Update profile-setup.sh
    Added confirmation for profile creation

--- profile-setup.sh
+++ profile-setup.sh
@@ -9,12 +9,27 @@ echo "[*] Enter Password: "
 read pw
 echo "[*] Enter Path to directory: "
 read path
-cd /home/$USER/.cend
-touch user-$prof.txt
-touch profiles.txt
-echo $prof >> profiles.txt
-echo $prof >>user-$prof.txt
-echo $ip >>user-$prof.txt
-ehco $un >>user-$prof.txt
-echo $pw >>user-$prof.txt
-echo $path >>user-$prof.txt
+echo "[!} Your profile has the following credentials:"
+echo "[!] Name: $prof IP: $ip Username: $un Password: $pw Path: $path"
+echo "[!] Would you like to save this profile? (y/n)"
+read answer
+if [ $answer = "y" ]; then
+        cd /home/$USER/.cend
+        touch user-$prof.txt
+        touch profiles.txt
+        echo $prof >> profiles.txt
+        echo $prof >>user-$prof.txt
+        echo $ip >>user-$prof.txt
+        echo $un >>user-$prof.txt
+        echo $pw >>user-$prof.txt
+        echo $path >>user-$prof.txt
+        cd
+else
+        echo "[!] Your profile will not be saved"
+fi
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                 

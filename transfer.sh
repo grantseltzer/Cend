bc181384cf5c669d2abb1277266f75a680227a5b
Grant Selter Rchman <grantseltzer@gmail.com>
2015-06-04 00:12:49 -0400

    Update transfer.sh
    Change it so that the command takes a parameter for the file instead of selecting it from a menu.

--- transfer.sh
+++ transfer.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 cwd=$(pwd)
-echo "[*] Please select file to secure copy"
-ls
-read -e file
-FileFindTest="$(find "$file")"
-echo "$FileFindTest"
 echo "[*] Please select remote profile: "
 cd /home/$USER/.cend/profiles
 ls
@@ -13,4 +8,4 @@ ProfFindTest="$(find "$profile")"
 echo "$ProfFindTest"
 profileInfo=$(<$profile)
 IFS=" " read prof ip un pw path<<< $profileInfo
-sshpass -p $pw scp -r $cwd/$file $un@$ip:$path
+sshpass -p $pw scp -r $cwd/$1 $un@$ip:$path

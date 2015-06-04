fedc88f234c54fefd9c8fa698c13998fcb9fc2fd
Grant Selter Rchman <grantseltzer@gmail.com>
2015-06-03 21:12:16 -0400

    Update transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+cwd=$(pwd)
 echo "[*] Please select file to secure copy"
 ls
-cwd=$(pwd)
-read file
-echo "[*] Please select remote profile: " 
-cat /home/$USER/.cend/profiles.txt
-read profile
-cd /home/$USER/.cend
-profileInfo=$(<user-$profile.txt)
+read -e file
+FileFindTest="$(find "$file")"
+echo "$FileFindTest"
+echo "[*] Please select remote profile: "
+cd /home/$USER/.cend/profiles
+ls
+read -e profile
+ProfFindTest="$(find "$profile")"
+echo "$ProfFindTest"
+profileInfo=$(<$profile)
 IFS=" " read prof ip un pw path<<< $profileInfo
 sshpass -p $pw scp -r $cwd/$file $un@$ip:$path

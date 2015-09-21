be45edb96861b1d1425df39239dfd5523f26508d
Grant Seltzer Richman <grantseltzer@gmail.com>
2015-09-21 11:47:39 -0400

    Update transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-cwd=$(pwd)
-echo "[*] Please select remote profile: "
-cd /home/$USER/.cend/profiles
-ls
-read -e profile
-ProfFindTest="$(find "$profile")"
-echo "$ProfFindTest"
-profileInfo=$(<$profile)
-IFS=" " read prof ip un pw path<<< $profileInfo
-sshpass -p $pw scp -r $cwd/$1 $un@$ip:$path
+cwd=$(pwd) #set variable cwd to the present working directory
+echo "[*] Please select remote profile: " #prompt the user for a profile to cend the file to
+cd /home/$USER/.cend/profiles #change directory to profiles directory, where all the profiles are saved as txt files
+ls #list all the profiles
+read -e profile # read in the desired profile
+ProfFindTest="$(find "$profile")" #Tab completion
+echo "$ProfFindTest" #Tab Completion
+profileInfo=$(<$profile) #Tab Completion
+IFS=" " read prof ip un pw path<<< $profileInfo # Take all of the information from profile directory and store in vars
+sshpass -p $pw scp -r $cwd/$1 $un@$ip:$path #scp the files using sshpass and scp command

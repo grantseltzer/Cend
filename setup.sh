2b0d745b38818f560c8cfbd47356903adc16404b
Grant Seltzer Richman <grantseltzer@gmail.com>
2015-09-21 11:52:06 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-mkdir /home/$USER/.cend
+mkdir /home/$USER/.cend    #Creates .cend folder for the bash scripts to be saved 
 mkdir /home/$USER/.cend/profiles
-cp profile-setup.sh /home/$USER/.cend
+cp profile-setup.sh /home/$USER/.cend # copy files over to the .cend folder
 cp transfer.sh /home/$USER/.cend
-echo "alias cend='bash /home/$USER/.cend/transfer.sh'" >> /home/$USER/.bashrc
+echo "alias cend='bash /home/$USER/.cend/transfer.sh'" >> /home/$USER/.bashrc  # create alias's for the scripts to be used 
 echo "alias cendProfile='bash /home/$USER/.cend/profile-setup.sh'" >> /home/$USER/.bashrc
-sudo apt-get install sshpass
+sudo apt-get install sshpass # install sshpass (obviously for debian/ubuntu)
